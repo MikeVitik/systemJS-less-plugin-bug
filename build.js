f82d089cdffb2cbd77ea8bb158a60b30569d6728
Mike Vitik <vitikm@gmail.com>
2016-10-21 12:39:50 +0300

    initial

---
+++ build.js
@@ -0,0 +1,13 @@
+System.register("tmp.less!npm:systemjs-less-plugin@1.8.4.js", [], function() { return { setters: [], execute: function() {} } });
+
+System.registerDynamic("tmp.js", ["tmp.less!npm:systemjs-less-plugin@1.8.4.js"], false, function ($__require, $__exports, $__module) {
+  var _retrieveGlobal = System.get("@@global-helpers").prepareGlobal($__module.id, null, null);
+
+  (function ($__global) {})(this);
+
+  return _retrieveGlobal();
+});
+System.register('tmp.less!npm:systemjs-less-plugin@1.8.4.js', [], false, function() {});
+(function(c){var d=document,a='appendChild',i='styleSheet',s=d.createElement('style');s.type='text/css';d.getElementsByTagName('head')[0][a](s);s[i]?s[i].cssText=c:s[a](d.createTextNode(c));})
+(".drop-down-icon {\n  background-image: url(D:\\tmp\\LessUrl/data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAA8AAAAPCAYAAAA71pVKAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAADsMAAA7DAcdvqGQAAAAYdEVYdFNvZnR3YXJlAHBhaW50Lm5ldCA0LjAuNWWFMmUAAABISURBVDhPY2hoaPhPLgZrBgIGUvGoZihOTEzEGrIgcWR1IDGsNqMbgK4RhHFqBmGYAdg0gjBezSCMSyMIE9SMDw91zeThhv8Ami96gQcIp6MAAAAASUVORK5CYII=);\n}\n");
+//# sourceMappingURL=build.js.map
